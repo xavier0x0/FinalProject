23c5a918bf00e41bf5020efa73088c8b24244cae
Bryce <bsumile@uncc.edu>
2024-12-02 15:15:41 -0500

    commit

---
+++ api/routers/users.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends, FastAPI, status, Response
+from sqlalchemy.orm import Session
+from ..controllers import order_details as controller
+from ..schemas import order_details as schema
+from ..dependencies.database import engine, get_db
+
+router = APIRouter(
+    tags=['Users'],
+    prefix="/users"
+)
+
+
+@router.post("/", response_model=schema.OrderDetail)
+def create(request: schema.OrderDetailCreate, db: Session = Depends(get_db)):
+    return controller.create(db=db, request=request)
